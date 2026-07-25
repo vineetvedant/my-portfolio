c81d104efbcfee2e7c081738249e76497e9a3f41
vedant singh <singhvineetvedant@gmail.com>
2026-07-25 22:04:35 +0530

    Fix routing by configuring BrowserRouter basename

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <LoadingScreen />
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/my-portfolio">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
