caa66e0d893362791e75a1bd0fe14080de0aa46a
vedant singh <singhvineetvedant@gmail.com>
2026-07-28 00:52:44 +0530

    Add Cloudflare-compatible deployment and SEO files

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <LoadingScreen />
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/my-portfolio">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
